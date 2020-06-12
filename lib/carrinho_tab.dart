a6081f3a61b7da6451779cde8a251e50f094808f
Ivan Wilhelm <ivan.whm@icloud.com>
2020-06-12 08:15:28 -0300

    Lab5: Adicionar as informações do cliente

--- lib/carrinho_tab.dart
+++ lib/carrinho_tab.dart
@@ -15,14 +15,125 @@ class _CarrinhoTabState extends State<CarrinhoTab> {
   Widget build(BuildContext context) {
     return Consumer<ModeloEstadoApp>(
       builder: (context, model, child) {
-        return const CustomScrollView(
+        return CustomScrollView(
           slivers: <Widget>[
-            CupertinoSliverNavigationBar(
-              largeTitle: Text('Carrinho de Compras'),
+            const CupertinoSliverNavigationBar(
+              largeTitle: Text('Carrinho'),
             ),
+            SliverSafeArea(
+              top: false,
+              minimum: const EdgeInsets.only(top: 4),
+              sliver: SliverList(
+                delegate: _construirFormulario(model),
+              ),
+            )
           ],
         );
       },
     );
+  }
+
+  String nome;
+  String email;
+  String localizacao;
+  String pin;
+  DateTime dateTime = DateTime.now();
+
+  Widget _construirCampoNome() {
+    return CupertinoTextField(
+      prefix: const Icon(
+        CupertinoIcons.person_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      autocorrect: false,
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Nome',
+      onChanged: (novoNome) {
+        setState(() {
+          nome = novoNome;
+        });
+      },
+    );
+  }
+
+  Widget _construirCampoEmail() {
+    return const CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.mail_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'E-mail',
+    );
+  }
+
+  Widget _construirCampoLocalizacao() {
+    return const CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.location_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Localização',
+    );
+  }
+
+  SliverChildBuilderDelegate _construirFormulario(ModeloEstadoApp modelo) {
+    return SliverChildBuilderDelegate((context, index) {
+      switch (index) {
+        case 0:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: _construirCampoNome(),
+          );
+        case 1:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: _construirCampoEmail(),
+          );
+        case 2:
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: _construirCampoLocalizacao(),
+          );
+        default:
+        // Do nothing. For now.
+      }
+      return null;
+    });
   }
 }
